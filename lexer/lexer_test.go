6578e86ce4d2370c612e1c5324cdd7f59317f324
MakotoNakai <macofandohr1812@gmail.com>
2022-01-08 13:31:00 +0900

    First test passed

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := "=+(){},;"
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -24,7 +24,7 @@ func TestNextToken(t *testing.T) {
 		{token.EOF, ""},
 	}
 
-	l := (input)
+	l := New(input)
 	for i, tt := range tests {
 		tok := l.NextToken()
 
